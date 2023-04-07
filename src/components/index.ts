ddaf57e5da6281658feb9d83a809bc8468092204
Na-hyunwoo <dusdjeks@naver.com>
2023-04-07 21:33:02 +0900

    build 에러 처리

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-import Introduction from "./Introduction";
-import MainSubject from "./MainSubject";
-import Conclusion from "./Conclusion";
-import Editor from "./Editor";
-
-export {Introduction, MainSubject, Conclusion, Editor}
